fec525ed20756ce3ea0b2954f16c07c40117ffa8
Angela <angela@londonappbrewery.com>
2018-07-03 17:15:58 +0100

    Initial Commit

---
+++ BitcoinTicker/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  BitcoinTicker
+//
+//  Created by Angela Yu on 23/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    var finalURL = ""
+
+    //Pre-setup IBOutlets
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    
+    //TODO: Place your 3 UIPickerView delegate methods here
+    
+    
+    
+
+    
+    
+    
+//    
+//    //MARK: - Networking
+//    /***************************************************************/
+//    
+//    func getWeatherData(url: String, parameters: [String : String]) {
+//        
+//        Alamofire.request(url, method: .get, parameters: parameters)
+//            .responseJSON { response in
+//                if response.result.isSuccess {
+//
+//                    print("Sucess! Got the weather data")
+//                    let weatherJSON : JSON = JSON(response.result.value!)
+//
+//                    self.updateWeatherData(json: weatherJSON)
+//
+//                } else {
+//                    print("Error: \(String(describing: response.result.error))")
+//                    self.bitcoinPriceLabel.text = "Connection Issues"
+//                }
+//            }
+//
+//    }
+//
+//    
+//    
+//    
+//    
+//    //MARK: - JSON Parsing
+//    /***************************************************************/
+//    
+//    func updateWeatherData(json : JSON) {
+//        
+//        if let tempResult = json["main"]["temp"].double {
+//        
+//        weatherData.temperature = Int(round(tempResult!) - 273.15)
+//        weatherData.city = json["name"].stringValue
+//        weatherData.condition = json["weather"][0]["id"].intValue
+//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
+//        }
+//        
+//        updateUIWithWeatherData()
+//    }
+//    
+
+
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,17 @@
+
+![App Brewery Banner](Documentation/AppBreweryBanner.png)
+
+#  Bitcoin Ticker
+
+## Our Goal
+
+The objective of this challenge is to apply the skills you learned in the Clima tutorial and make a Bitcoin price reporter. We are going to revisit how to use Cocoapods, networking calls and JSON parsing. This will be the most advanced challenge you will face to date. We believe in you!
+
+## What you will create
+
+We’re going to make a Bitcoin price ticker. The app will give you the latest Bitcoin prices in all the popular, traded currencies, so you can track how well your Bitcoin investments are doing on the go.
+
+
+>This is a companion project to The App Brewery's Complete App Developement Bootcamp, check out the full course at [www.appbrewery.co](https://www.appbrewery.co/)
+
+![End Banner](Documentation/readme-end-banner.png)
